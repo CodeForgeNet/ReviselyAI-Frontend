39bf3beab770e17c5f9f832701eaa80349299c87
CodeForgeNet <codeforgenet@icloud.com>
2025-10-18 19:26:57 +0530

    update code

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,7 @@ import "./index.css";
 import "./styles/pdf-viewer.css";
 import { pdfjs } from 'react-pdf';
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.mjs',
-  import.meta.url,
-).toString();
+pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
