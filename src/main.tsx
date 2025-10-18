d4577d941872b472fb6232a1962ac4451c5554f2
CodeForgeNet <codeforgenet@icloud.com>
2025-10-18 19:15:04 +0530

    update code

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,12 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import "./styles/pdf-viewer.css";
+import { pdfjs } from 'react-pdf';
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.min.mjs',
+  import.meta.url,
+).toString();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
